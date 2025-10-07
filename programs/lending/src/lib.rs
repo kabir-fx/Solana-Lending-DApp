f8c2574c5aa06e44f777ca507097f6714cf897f7
Kabir <vatshivam49888@gmail.com>
2025-10-08 02:02:35 +0530

    Implement Deposit Instruction

--- programs/lending/src/lib.rs
+++ programs/lending/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use instructions::admin::*;
+use instructions::deposit::*;
 
 mod instructions;
 mod state;
@@ -16,6 +17,10 @@ pub mod lending {
 
     pub fn initialize_account(ctx: Context<InitializeAccount>, usdc_address: Pubkey) -> Result<()> {
         process_initialize_account(ctx, usdc_address)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount_to_deposit: u64) -> Result<()> {
+        process_deposit(ctx, amount_to_deposit)
     }
 }
 
